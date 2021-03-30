01b85846bda7df72b7e66f5a3e990f39da538208
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-03-30 04:33:31 +0200

    polish filters & add reset button

--- frontend/src/components/characters.js
+++ frontend/src/components/characters.js
@@ -14,18 +14,66 @@ const Characters = (props) => {
 	}, [])
 
 	useEffect(() => {
-		let humanFilter = data.filter((item) => {
-			return item.human === isHuman;
-		})
-		setFiltered(humanFilter)
+		let humanFilter = data;
+		if (filtered && isHuman !== null && gender !== "") {
+			humanFilter = humanFilter.filter((fil, index) => {
+				return (
+					fil.human === isHuman && fil.call.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase()) && fil.gender === gender
+				);
+			});
+			setFiltered(humanFilter)
+		} else if (filtered && isHuman !== null) {
+			humanFilter = humanFilter.filter((fil, index) => {
+				return (
+					fil.human === isHuman && fil.call.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase())
+				);
+			});
+			setFiltered(humanFilter);
+		} else if (gender !== "" && isHuman !== null) {
+			humanFilter = humanFilter.filter((fil, index) => {
+				return (
+					fil.human === isHuman && fil.gender === gender
+				);
+			});
+			setFiltered(humanFilter);
+		} else if (isHuman !== null) {
+			humanFilter = humanFilter.filter((item) => {
+				return item.human === isHuman;
+			})
+			setFiltered(humanFilter);
+		}
+
 	}, [isHuman])
 
 	useEffect(() => {
-		let genderFilter = data.filter((item) => {
-			return item.gender === gender;
-		})
-
-		setFiltered(genderFilter)
+		let genderFilter = data;
+		if (filtered && isHuman !== null && gender !== "") {
+			genderFilter = genderFilter.filter((fil, index) => {
+				return (
+					fil.human === isHuman && fil.call.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase()) && fil.gender === gender
+				);
+			});
+			setFiltered(genderFilter)
+		} else if (filtered && gender !== "") {
+			genderFilter = genderFilter.filter((fil, index) => {
+				return (
+					fil.gender === gender && fil.call.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase())
+				);
+			});
+			setFiltered(genderFilter);
+		} else if (isHuman !== null && gender !== "") {
+			genderFilter = genderFilter.filter((fil, index) => {
+				return (
+					fil.gender === gender && fil.human === isHuman
+				);
+			});
+			setFiltered(genderFilter);
+		} else if (gender !== "") {
+			genderFilter = genderFilter.filter((item) => {
+				return item.gender === gender;
+			})
+			setFiltered(genderFilter);
+		}
 	}, [gender])
 
 
@@ -39,14 +87,72 @@ const Characters = (props) => {
 
 	const editSearch = (e) => {
 		let keywords = e.target.value.toLowerCase();
-		if (keywords.length === 0) {
-			setFiltered(data)
-		} else {
-			let filtered = data.filter((item) => {
+		let filtered = data;
+
+		if (keywords.length !== 0 && isHuman !== null && gender !== "") {
+			filtered = filtered.filter((item) => {
+				return (
+					item.human === isHuman && item.call.toLowerCase().includes(keywords) && item.gender === gender
+				);
+			})
+			setFiltered(filtered)
+		} else if (keywords.length !== 0 && isHuman !== null) {
+			filtered = filtered.filter((item) => {
+				return (
+					item.human === isHuman && item.call.toLowerCase().includes(keywords)
+				);
+			})
+			setFiltered(filtered)
+		} else if (keywords.length !== 0 && gender !== "") {
+			filtered = filtered.filter((item) => {
+				return (
+					item.gender === gender && item.call.toLowerCase().includes(keywords)
+				);
+			})
+			setFiltered(filtered)
+		} else if (keywords.length !== 0) {
+			filtered = filtered.filter((item) => {
 				return item.call.toLowerCase().indexOf(keywords) > - 1;
 			})
 			setFiltered(filtered)
+		} else if (keywords.length === 0 && gender !== "" && isHuman !== null) {
+			filtered = filtered.filter(fil => {
+				return (
+					fil.gender === gender && fil.human === isHuman
+				);
+			});
+			setFiltered(filtered);
+		} else if (keywords.length === 0 && gender !== "") {
+			filtered = filtered.filter(fil => {
+				return (
+					fil.gender === gender
+				);
+			});
+			setFiltered(filtered);
+		} else if (keywords.length === 0 && isHuman !== null) {
+			filtered = filtered.filter(fil => {
+				return (
+					fil.human === isHuman
+				);
+			});
+			setFiltered(filtered);
+		} else if (keywords.length === 0 && isHuman == null && gender === "") {
+			setFiltered(filtered);
 		}
+	}
+
+	const resetAll = () => {
+		let radios = document.querySelectorAll(".humanFilter")
+		for (let i = 0; i < radios.length; i++) {
+			radios[i].checked = false;
+		}
+		setIsHuman(null);
+
+		document.getElementById("textFilter").value = "";
+		setFiltered(data)
+
+		document.getElementById("genderFilter").value = "";
+		setGender("");
 	}
 
 	return (
@@ -59,7 +165,6 @@ const Characters = (props) => {
 						<h1>Prince of Persia</h1>
 						<h2>Characters</h2>
 					</div>
-					<h2>Filter by:</h2>
 					<div id="filters">
 						<span id="radios">
 							<h3>Type:</h3>
@@ -72,12 +177,10 @@ const Characters = (props) => {
 								<span className="checkmark"></span>
 							</label>
 						</span>
-						<span className="or">or</span>
 						<span>
 							<h3>Name:</h3>
 							<input type="text" id="textFilter" onChange={(e) => editSearch(e)} />
 						</span>
-						<span className="or">or</span>
 						<span>
 							<h3>Gender:</h3>
 							<select name="gender" id="genderFilter" onChange={(event) => setGender(parseInt(event.target.value))} value={gender} >
@@ -87,6 +190,9 @@ const Characters = (props) => {
 								<option value="3">Other</option>
 							</select>
 						</span>
+					</div>
+					<div id="resetCont">
+						<button id="resetBtn" onClick={resetAll}>Reset all filter</button>
 					</div>
 					<div id="content">
 						{filtered ?
